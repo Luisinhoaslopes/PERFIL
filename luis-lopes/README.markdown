30d3da900b67e70749aed9a6425317f8fdfafd0c
Luisinhoaslopes <101285272+Luisinhoaslopes@users.noreply.github.com>
2022-03-17 17:48:12 -0300

    ISSO

---
+++ luis-lopes/README.markdown
@@ -0,0 +1,5 @@
+# luis lopes
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/s-lopess/pen/WNdrMva](https://codepen.io/s-lopess/pen/WNdrMva).
+
+
---
+++ luis-lopes/dist/script.js
@@ -0,0 +1,11 @@
+document.getElementsByTagName;
+        (function() {
+  var docElement = document.documentElement;
+  var classRE = new RegExp('(^|\\s)no-js(\\s|$)');
+  var className = docElement.className;
+  docElement.className = className.replace(classRE, '$1js$2');
+          
+document.getElementById:
+    (fuctin
+   
+    )
